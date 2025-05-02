922bd304f00ec150d9570a373d857de9886e44ad
Iyagkordai <suphichaya.khanereo@gmail.com>
2025-05-02 11:49:00 +0700

    commit

--- klue_page/klue.js
+++ klue_page/klue.js
@@ -1,26 +1,3 @@
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".navmenu");
-
-window.addEventListener("scroll", () => {
-    let current = "";
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-
-        if (window.scrollY >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href").includes(current)) {
-            link.classList.add("active");
-        }
-    });
-});
-
 const munu = document.querySelector("#menu_icon");
 const navbar = document.querySelector(".navbar");
 
