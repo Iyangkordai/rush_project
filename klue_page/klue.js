7978d26cb2648b82c1f6842c18b90a61f983ddf2
Iyagkordai <suphichaya.khanereo@gmail.com>
2025-05-02 11:24:17 +0700

    commit

--- klue_page/klue.js
+++ klue_page/klue.js
@@ -20,3 +20,11 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+const munu = document.querySelector("#menu_icon");
+const navbar = document.querySelector(".navbar");
+
+munu.addEventListener("click", () =>{
+    munu.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+})
